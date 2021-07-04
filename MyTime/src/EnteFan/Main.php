d9e8f13f1ae20bcff8c33a7568d7bc1e2fed2061
EnteFanPHP <78978307+EnteFanPHP@users.noreply.github.com>
2021-07-04 18:55:28 +0200

    Main.php

---
+++ MyTime/src/EnteFan/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EnteFan;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+
+class Main extends PluginBase{
+  
+  public function onCommand(CommandSender $sender, Command $cmd, string $string, array $args):bool{
+    if($cmd->getName() == "mytime"){
+      $date = date("d.m.Y");
+      $day = date("l");
+      
+      $sender->sendMessage("Today is a: " . $day);
+      $sender->sendMessage("Today is the: ". $date);
+    }
+    return true;
+  }
+  
+  
+}
